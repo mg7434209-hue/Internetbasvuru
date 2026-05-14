d8851a124d43f186720d8da28052ee0bb1926264
sonsuz07 <mg7434209@gmail.com>
2026-05-14 12:22:17 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,7 +28,7 @@ export default function Footer() {
             <div className="inline-flex items-center gap-2 bg-white/5 border border-white/10 rounded-full px-3 py-1.5 mb-4">
               <Award className="w-3.5 h-3.5 text-brand-500" />
               <span className="text-[11px] font-bold tracking-wide text-white/80">
-                Bayi Kodu: B9613
+               
               </span>
             </div>
  
